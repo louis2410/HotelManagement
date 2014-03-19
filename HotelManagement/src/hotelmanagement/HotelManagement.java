ae7c8fb58e6dfe597c4017a93b5a9ee8cb8de28f
xyzwlxp <249363663@qq.com>
2014-03-19 21:05:58 +0800

    a

--- HotelManagement/src/hotelmanagement/HotelManagement.java
+++ HotelManagement/src/hotelmanagement/HotelManagement.java
@@ -11,7 +11,7 @@ package hotelmanagement;
  * @author Louis
  */
 public class HotelManagement {
-//////sadasdasdasdasda hello
+//////sadasdasdasdasda helloa
     /**
      * @param args the command line arguments
      */
