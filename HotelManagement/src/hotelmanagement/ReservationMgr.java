89b1ec331478360cf9837c06ea17f3e0f85f1438
louis2410 <louis2410@hotmail.com>
2014-04-10 19:48:39 +0800

    Added XML functions for manager class

--- HotelManagement/src/hotelmanagement/ReservationMgr.java
+++ HotelManagement/src/hotelmanagement/ReservationMgr.java
@@ -1,5 +1,29 @@
+package hotelmanagement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.persistence.FilePersistenceStrategy;
+import com.thoughtworks.xstream.persistence.PersistenceStrategy;
+import com.thoughtworks.xstream.persistence.XmlArrayList;
+
+import hotelmanagement.XML.Author;
+
+import java.io.File;
+import java.util.List;
+
+
+
 public class ReservationMgr implements Manager {
 
+     //Attributes
+    private ArrayList<Reservation> arrayReservation;
+    
+    public ReservationMgr(){
+        
+        arrayReservation = new ArrayList<Reservation>();
+    }
+    
 	/**
 	 * 
 	 * @param reservation
@@ -36,4 +60,80 @@ public class ReservationMgr implements Manager {
 		throw new UnsupportedOperationException();
 	}
 
+        
+        //Print all reservations
+        public void printReservations(){
+              for(Iterator it = arrayReservation.iterator();it.hasNext();){
+                Reservation reservation = (Reservation)it.next();
+                System.out.println(reservation.getResId());
+                
+            }
+            
+        }
+        
+        
+          //Creates XML files from an array of Guest Objects
+        //Do note the folder directory
+        public void createToFile()
+        {
+            
+            // prepares the file strategy to directory 
+		PersistenceStrategy strategy = new FilePersistenceStrategy(new File("/Users/Louis/HotelManagement/HotelManagement/Reservation"));
+		// creates the list:
+		List list = new XmlArrayList(strategy);
+                
+                //for each guest in arrayGuest, create one xml file
+		 for(Iterator it = arrayReservation.iterator();it.hasNext();){
+                Reservation reservation = (Reservation)it.next();
+                list.add(reservation);
+                      
+                }
+
+           System.out.println("Reservations to XML Complete");
+        }
+        
+        //Delete all xml files from specified folder,run this method before createToFile else u will get duplicates
+        public boolean DeleteFromFile(){
+            
+            // prepares the file strategy to directory 
+		PersistenceStrategy strategy = new FilePersistenceStrategy(new File("/Users/Louis/HotelManagement/HotelManagement/Reservation"));
+		// creates the list:
+		List list = new XmlArrayList(strategy);
+                
+                //for each guest in Guest folder, delete each
+		 for(Iterator it = list.iterator();it.hasNext();){
+                Reservation reservation = (Reservation)it.next();
+                it.remove();
+                }
+            
+
+            return true;
+        }
+         
+          //Reads from the reservation Folder and then creates adds reservation Objects into arrayReservation
+        public boolean retrieveFromFile(){
+            
+             // prepares the file strategy to directory 
+		PersistenceStrategy strategy = new FilePersistenceStrategy(new File("/Users/Louis/HotelManagement/HotelManagement/Reservation"));
+		// creates the list:
+		List list = new XmlArrayList(strategy);
+            
+                
+                //for each reservation in reservation folder, creates an reservation object in reservationarray
+             for(Iterator it = list.iterator();it.hasNext();){
+                Reservation reservation = (Reservation)it.next();
+                arrayReservation.add(reservation);
+                }
+            
+            return true;
+        }
+        
+        public void setup()
+        {
+             this.retrieveFromFile();
+            this.DeleteFromFile();
+            this.printReservations();
+            this.createToFile();
+        }
+        
 }
